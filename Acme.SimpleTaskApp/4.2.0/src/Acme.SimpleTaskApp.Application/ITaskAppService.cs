09041a31f4ab68967b55dfd5e1c62a832f7b684c
凯夫 邓 <943514827@qq.com>
2019-10-31 22:51:10 +0800

    create a taskapp.

---
+++ Acme.SimpleTaskApp/4.2.0/src/Acme.SimpleTaskApp.Application/ITaskAppService.cs
@@ -0,0 +1,15 @@
+﻿using Abp.Application.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Acme.SimpleTaskApp.Tasks;
+using System.Threading.Tasks;
+using Abp.Application.Services.Dto;
+
+namespace Acme.SimpleTaskApp
+{
+    public interface ITaskAppService : IApplicationService
+    {
+        Task<ListResultDto<TaskListDto>> GetAll(GetAllTasksInput input);
+    }
+}
---
+++ Acme.SimpleTaskApp/4.2.0/src/Acme.SimpleTaskApp.EntityFrameworkCore/EntityFrameworkCore/SimpleTaskAppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Abp.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Acme.SimpleTaskApp.Tasks;
+namespace Acme.SimpleTaskApp.EntityFrameworkCore
+{
+    public class SimpleTaskAppDbContext : AbpDbContext
+    {
+        public DbSet<Task> Tasks { get; set; }
+
+        public SimpleTaskAppDbContext(DbContextOptions<SimpleTaskAppDbContext> options)
+            : base(options)
+        {
+
+        }
+    }
+}
